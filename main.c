74dedd3b573cf43ab7fca42d3860076fc8cb734f
danik529 <lsy12011@naver.com>
2018-11-22 15:24:45 +0900

    gyeasan

--- main.c
+++ main.c
@@ -6,17 +6,21 @@
 int main(int argc, char *argv[]) {
 	
  char src[100]="Programming course";
- char dst[100];
+ //char dst[100];
  int i=0;
  
  while (src[i]!='\0')
  {
- 	dst[i]=src[i];
- 	i++;
+ // dst[i] = src[i];
+    i++;
  }
- dst[i]='\0';
+ //dst[i]='\0';
  
- printf("dst : %s\n", dst);
+ //pritnf("dst : %s\n", dst)
+ printf("%s (%i)\n", src, i);
+ printf("%i", strlen(src) );
+ 
+ //strcpy(dst, src);
    
 	return 0;
 }
